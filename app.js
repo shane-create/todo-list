3cb9a52e151ea9fade5f114c1f8e1d9ae56fb62e
Shaila Kafi Khan <shai0136@g2650.dk>
2020-08-06 19:53:21 +0200

    Completed with comments and all of that

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
+//My node modules
 const express = require("express");
 const bodyParser = require("body-parser");
 
+//My Owm module
+const date = require(__dirname + "/date.js");
+
+//App contains express
 const app = express();
 
+//My two list arrays
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+//Setting the view engine and ejs
 app.set('view engine', 'ejs');
 
+//Makeing my static files public
 app.use(express.static("public"));
 
+//Getting body parser
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-
+//Creating a function to display on my home route
 app.get("/", function(req, res) {
 
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
+//My own modue required
+  let day = date.getDate();
 
-  let day = today.toLocaleDateString("en-US", options)
-
-  res.render('list', {toDay: day, newListItems:items});
+//Rendering the html with ejs templating
+  res.render('list', {listTitle: day, newListItems:items});
 });
 
+//Posting my js to the home route
 app.post("/", function(req, res){
+
+//Letting the item equal the newItem input
   let item  = req.body.newItem;
 
-  items.push(item);
-
-  res.redirect("/");
+//Seeing if the route is work or just the default
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work")
+  } else{
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
+//Creating a function to display on my /work route
+app.get("/work", function(req, res){
+  res.render('list', {listTitle: "Work", newListItems: workItems});
+});
 
+//Again posting to my work route
+app.post("/work", function(req, res){
+  let item  = req.body.newItem;
+
+//pushing my items
+  items.push(item);
+
+//Redirecting everything to my /work page
+  res.redirect("/work");
+});
+
+//The port its being listend on
 app.listen(3000, function() {
   console.log("The server has started on port 3000")
 });
